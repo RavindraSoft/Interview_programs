4be706a3a723b314e015dc0941365bbc1b094f05
Nitesh Pawar <niteshpawar019@gmail.com>
2023-09-03 19:43:45 +0530

    test 2

--- sort_array_without_inbuild fucntion.php
+++ sort_array_without_inbuild fucntion.php
@@ -1,5 +1,5 @@
 <?php
-$arr = [9,8,7,5];
+$arr = [1,2,9,8,7,5];
 $n=0;
 for($i=0; @$arr[$i]!=''; $i++)
     $n++;
@@ -14,8 +14,7 @@ echo "<br>";
 
 for($i=0; $i<$n-1; $i++){
     for($j=0; $j<$n-1; $j++){
-//        echo $i." ".$j." - ".implode(",",$arr)."<br>";
-        if($arr[$j] < $arr[$j+1]){
+        if($arr[$j] > $arr[$j+1]){
             $temp = $arr[$j];
             $arr[$j]=$arr[$j+1];
             $arr[$j+1]=$temp;
